25bfe8d05eb54228fd63313065bfbe2f4ae8d96c
Nick Mele <nickmele26@gmail.com>
2015-05-17 21:49:03 -0400

    finished upnp and nupnp searches

--- test/index.js
+++ test/index.js
@@ -1,22 +1,43 @@
 var should = require('chai').should();
 var Hue    = require('../lib');
 
-var hue = new Hue({});
+var hue = new Hue({
+  autoDiscover: false
+});
 
 describe('Hue API', function() {
 
-  describe('discover', function() {
+  describe('Discover', function() {
 
     it('should perform a search on the network for the bridge(s)', function (done) {
-
-      hue.discover().then(function(results, error) {
-        console.log(results);
-        results.should.be.an('array');
-        done();
-      });
-
+      hue.discover().then(validateSearchResponse(done));
     });
 
   });
+
+  describe('UPnP Search', function() {
+
+    it('should be able to search the network via UPnP for a bridge', function(done) {
+      hue._upnpSearch().then(validateSearchResponse(done));
+    });
+
+  });
+
+  describe('N-UPnP Search', function() {
+
+    it('should be able to search the network via N-UPnP for a bridge', function(done) {
+      hue._nupnpSearch().then(validateSearchResponse(done));
+    });
+
+  });
+
+  function validateSearchResponse(done) {
+    return function (results) {
+      results.should.be.an('array')
+        .with.deep.property('[0]')
+        .that.includes.keys('id', 'internalipaddress');
+      done();
+    };
+  };
 
 });
